b49514501c1c4fc3cec8cbf5dbde3d374f82ab28
Pierre Garrigues <141630659+Lapinsour@users.noreply.github.com>
2026-04-20 16:08:33 +0200

    Update app.py

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -21,36 +21,19 @@ def render_card(card):
     color = colors.get(rarity, "#ffffff")
     shadow_strength = "0 0 20px" if rarity in ["epic", "legendary"] else "0 0 10px"
 
-    st.markdown(
-        f"""
-        <div style="
-            display:flex;
-            justify-content:space-between;
-            align-items:center;
-            margin:8px 0;
-        ">
-            <div style="
-                font-size:16px;
-                font-weight:bold;
-                color:white;
-            ">
-                ⚔ {card.get("power", "N/A")}
-            </div>
+    # badge rarity 
+    st.markdown( 
+        f""" <div style=" 
+        display:inline-block; 
+        padding:4px 10px; 
+        border-radius:8px; 
+        background-color:{color}; 
+        color:black; 
+        font-weight:bold; 
+        font-size:12px; "> {rarity} </div> """, 
+        unsafe_allow_html=True ) 
     
-            <div style="
-                padding:4px 10px;
-                border-radius:8px;
-                background-color:{color};
-                color:black;
-                font-weight:bold;
-                font-size:12px;
-            ">
-                {rarity}
-            </div>
-        </div>
-        """,
-        unsafe_allow_html=True
-    ) 
+    st.header(card["power"], text_alignment="right")
 
     st.markdown(
         f"""
