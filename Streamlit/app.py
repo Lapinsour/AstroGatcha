8197810dd6c579f632af6beb8ea8d66bec4ea334
Pierre Garrigues <141630659+Lapinsour@users.noreply.github.com>
2026-04-20 15:05:59 +0200

    Update app.py

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -28,3 +28,7 @@ def render_card(card):
 if st.button("🎴 Pull card"):
     r = requests.get(f"{API_URL}/pull", timeout=30)
     card = r.json()["result"]
+    st.image(card["image_url"])
+    st.write(card["title"])
+    st.write(card["rarity"])
+    st.write(card["power"])
