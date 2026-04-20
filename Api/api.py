46854d1738a177c07622aa3386458cb05540c22a
Pierre Garrigues <141630659+Lapinsour@users.noreply.github.com>
2026-04-20 13:57:47 +0200

    Create api.py

---
+++ Api/api.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from pymongo import MongoClient
+import random
+
+app = FastAPI()
+
+MONGO_URI = "mongodb://localhost:27017/"
+client = MongoClient(MONGO_URI)
+db = client["nasa"]
+
+# -------------------------
+# GET ALL CARDS
+# -------------------------
+@app.get("/cards")
+def get_cards():
+    return list(db.cards.find({}, {"_id": 0}))
+
+
+# -------------------------
+# GACHA PULL
+# -------------------------
+@app.get("/pull")
+def pull_card():
+    cards = list(db.cards.find({}, {"_id": 0}))
+
+    if not cards:
+        return {"error": "No cards found"}
+
+    card = random.choice(cards)
+    return {
+        "result": card
+    }
